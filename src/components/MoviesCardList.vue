96ee9f7842c84b94aa64a8f0c12a2696419bd37a
Jessica <jesscara.993@gmail.com>
2022-12-16 17:24:16 +0100

    created component with div testing api working

---
+++ src/components/MoviesCardList.vue
@@ -0,0 +1,23 @@
+<script >
+import { store } from '../store.js';
+export default {
+  data() {
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+  <div v-for="movie in store.moviesList" :key="movie.id">
+    <img :src="`https://image.tmdb.org/t/p/w300${movie.poster_path}`" alt="">
+    <h4>{{ movie.original_title }}</h4>
+    <h4>{{ movie.original_language }}</h4>
+    <h4>{{ movie.vote_average }}</h4>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
