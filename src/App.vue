b2ed59a6ec46083f6f715ad13cc4b0bf5fc935ca
Jessica <jesscara.993@gmail.com>
2022-12-16 17:22:59 +0100

    imported axios, store, method to get data from api

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+import MoviesCardList from './components/MoviesCardList.vue'
+
+import { store } from './store.js';
+
+export default {
+  components: {
+    MoviesCardList
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    getMovies() {
+      let myUrl = store.apiURL;
+      // per connettersi all api
+      axios
+        .get(myUrl)
+        .then(res => {
+          store.moviesList = res.data.results
+        }
+        )
+    }
+  },
+  mounted() {
+    this.getMovies();
+  }
+}
+
 </script>
 
 <template>
-  <HelloWorld />
+  <main>
+    <MoviesCardList />
+  </main>
 </template>
 
 <style lang="scss">
