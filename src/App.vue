a4c1d3ab817050b31ba866d68352ded41c411345
Jessica <jesscara.993@gmail.com>
2022-12-17 01:14:59 +0100

    changed myUrl to show movies based on user search

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script>
 import axios from 'axios';
 import MoviesCardList from './components/MoviesCardList.vue'
+import Header from './components/Header.vue';
 
 import { store } from './store.js';
 
 export default {
   components: {
-    MoviesCardList
+    MoviesCardList,
+    Header,
   },
   data() {
     return {
@@ -16,6 +18,12 @@ export default {
   methods: {
     getMovies() {
       let myUrl = store.apiURL;
+
+      // se l'utente scrive il nome di un film, myUrl viene sostituito dalla query del film cercato e simili
+      if (store.searchText !== '') {
+        myUrl = `https://api.themoviedb.org/3/search/movie?api_key=6fd82c7e095558dcca8a44519f3dc58a&query=${store.searchText}`
+      }
+
       // per connettersi all api
       axios
         .get(myUrl)
@@ -33,11 +41,12 @@ export default {
 </script>
 
 <template>
+  <Header @search="getMovies" />
   <main>
     <MoviesCardList />
   </main>
 </template>
 
 <style lang="scss">
-
+@use './styles/general.scss' as *;
 </style>
