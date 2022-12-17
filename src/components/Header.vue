bf72308b5184c46ca48c8dc547d2341f399d975b
Jessica <jesscara.993@gmail.com>
2022-12-17 01:03:54 +0100

    created search input and button + emit and v-model

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <h1>Boolflix</h1>
+
+      <div class="search_bar">
+        <!-- ricerca nell'api in base a cosa scrive l'utente -->
+        <input type="search" placeholder="Scrivi qualcosa..." v-model="store.searchText">
+        <button type="submit" @click.prevent="$emit('search')">
+          Cerca
+        </button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+.container {
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    color: $accent;
+  }
+}
+</style>
