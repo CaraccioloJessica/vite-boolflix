d8dc11fe11f9539d4f22b05f3076502109c6358c
Jessica <jesscara.993@gmail.com>
2022-12-19 13:11:00 +0100

    originaltitle hidden if is equal to tiltle

--- src/components/MoviesCardList.vue
+++ src/components/MoviesCardList.vue
@@ -36,12 +36,14 @@ export default {
       <img :src="movie.poster_path ? `https://image.tmdb.org/t/p/w185${movie.poster_path}` : 'img/image-not-found.jpg'"
         alt="">
       <!-- titolo film -->
-      <h4>{{ movie.title }}</h4>
+      <h4>Titolo: {{ movie.title }}</h4>
+      <h4 v-if="movie.title != movie.original_title">Titolo originale: {{ movie.original_title }}</h4>
       <!-- condizione per bandiera o stringa lingua -->
       <h5 :innerHTML="displayLanguage(movie.original_language)">
       </h5>
       <!-- votazione film -->
-      <div>
+      <div class="rating">
+        Voto:
         <!-- stampa stelle in base alla votazione -->
         <span v-for="star in rating(movie.vote_average)">&#9733;</span>
         <!-- stampa le rimanenti stelle vuote, facendo la differenza tra un intervallo di 5 numeri meno il voto arrotondato del metodo-->
@@ -51,16 +53,24 @@ export default {
 
     <!-- array delle serie tv -->
     <div class="card" v-for="tv in store.tvList" :key="tv.id">
+
       <!-- immagine -->
       <img :src="tv.poster_path ? `https://image.tmdb.org/t/p/w185${tv.poster_path}` : 'img/image-not-found.jpg'"
         alt="">
+
       <!-- titolo -->
-      <h4>{{ tv.name }}</h4>
+      <h4>Titolo: {{ tv.name }}</h4>
+
+      <!-- mostra titolo originale se diverso da titolo -->
+      <h4 v-if="tv.name != tv.original_name">Titolo originale: {{ tv.original_name }}</h4>
+
       <!-- condizione per bandiera o stringa lingua -->
       <h5 :innerHTML="displayLanguage(tv.original_language)">
       </h5>
+
       <!-- votazione serie -->
-      <div>
+      <div class="rating">
+        Voto:
         <span v-for="star in rating(tv.vote_average)">&#9733;</span>
         <span v-for="star in 5 - rating(tv.vote_average)">&#9734;</span>
       </div>
@@ -77,6 +87,14 @@ export default {
   .card {
     color: $text;
     width: 185px;
+
+    h4 {
+      font-size: 0.8rem;
+    }
+
+    .rating {
+      font-size: 0.8rem;
+    }
 
     img {
       width: 185px;
