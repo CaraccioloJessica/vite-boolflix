42524b8849a8947f0cc54c79a3aaec8c6f0b8c81
Jessica <jesscara.993@gmail.com>
2022-12-19 11:09:20 +0100

    added tv array

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { reactive } from 'vue';
 
 export const store = reactive({
   moviesList: [],
+  tvList: [],
   apiURL: 'https://api.themoviedb.org/3/movie/popular?api_key=6fd82c7e095558dcca8a44519f3dc58a&page=1', //chiamata all'api per film da visualizzare in prima pagina 
   searchText: ''
 });
